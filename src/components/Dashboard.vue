eeb0052798a377d122e5c53d071860062dbd0bc6
Alexis Couronne <alexis@skitoo.net>
2018-12-23 22:45:30 +0100

    feat: highlight current player on dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,10 +4,16 @@
       <h2>Scores</h2>
       <div class="scores-container">
         <div>
-          Black: {{blackPlayerScore}}
+          <h3 :class="{ currentPlayer: isBlackPlayerTurn }">
+            Black
+          </h3>
+          <div class="score-value">{{blackPlayerScore}}</div>
         </div>
         <div>
-          White: {{whitePlayerScore}}
+          <h3 :class="{ currentPlayer: isWhitePlayerTurn }">
+            White
+          </h3>
+          <div class="score-value">{{whitePlayerScore}}</div>
         </div>
       </div>
     </div>
@@ -16,6 +22,7 @@
 
 <script>
 import { mapGetters } from 'vuex';
+import { BLACK, WHITE } from '@/store/commons';
 
 export default {
   computed: {
@@ -23,6 +30,12 @@ export default {
       'blackPlayerScore',
       'whitePlayerScore',
     ]),
+    isBlackPlayerTurn() {
+      return this.$store.state.currentPlayer === BLACK;
+    },
+    isWhitePlayerTurn() {
+      return this.$store.state.currentPlayer === WHITE;
+    },
   },
 };
 </script>
@@ -35,10 +48,33 @@ export default {
 
 h2 {
   text-align: center;
+  margin-bottom: 0;
+}
+
+@keyframes blink {
+  0% { background: #fff; }
+  100% { background: #C4E79A; }
+}
+
+h3 {
+  padding: 6px 5px 3px;
+  border: 1px solid #94C160;
+  border-radius: 3px;
 }
 
 .scores-container {
   display: flex;
   justify-content: space-between;
+  text-align: center;
 }
+
+.currentPlayer {
+  font-weight: bold;
+  animation: blink 1s linear infinite;
+}
+
+.score-value {
+  font-size: 1.5em;
+}
+
 </style>
--- tests/unit/dashboard.spec.js
+++ tests/unit/dashboard.spec.js
@@ -17,4 +17,28 @@ describe('components::Dashboard', () => {
       expect(wrapper.findAll('.score').length).toEqual(1);
     });
   });
+  describe('isBlackPlayerTurn', () => {
+    it('should returns true if current player is BLACK', () => {
+      expect(wrapper.vm.isBlackPlayerTurn).toBeTruthy();
+      store.commit('changePlayer');
+      expect(wrapper.vm.isBlackPlayerTurn).toBeFalsy();
+    });
+  });
+  describe('isWhitePlayerTurn', () => {
+    it('should returns true if current player is WHITE', () => {
+      expect(wrapper.vm.isWhitePlayerTurn).toBeFalsy();
+      store.commit('changePlayer');
+      expect(wrapper.vm.isWhitePlayerTurn).toBeTruthy();
+    });
+  });
+  describe('hightlight current player', () => {
+    it('should hightlight current player on dashboard', () => {
+      const h3s = wrapper.findAll('h3');
+      expect(h3s.at(0).classes()).toContain('currentPlayer');
+      expect(h3s.at(1).classes()).not.toContain('currentPlayer');
+      store.commit('changePlayer');
+      expect(h3s.at(0).classes()).not.toContain('currentPlayer');
+      expect(h3s.at(1).classes()).toContain('currentPlayer');
+    });
+  });
 });
