f441898ebe8a52e2ef888c07140246d79e697145
Alexis Couronne <alexis@skitoo.net>
2018-12-23 11:41:41 +0100

    chore: remove useless code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
-import store from './store';
+import { createStore } from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  store,
+  store: createStore(),
   render: h => h(App),
 }).$mount('#app');
