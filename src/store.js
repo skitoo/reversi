cbd9f3654fbc54b6a8e8d2b85c3f5c68a8cab170
Alexis Couronne <alexis@skitoo.net>
2018-12-23 08:56:38 +0100

    feat: initialize default state data

--- src/store.js
+++ src/store.js
@@ -3,14 +3,35 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const INITIAL_BOARD = [
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 1, 2, 0, 0, 0,
+  0, 0, 0, 2, 1, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+export const BLACK = 1;
+export const WHITE = 2;
+
+
+export const state = {
+  board: INITIAL_BOARD,
+  currentPlayer: BLACK,
+};
+
+export const mutations = {
+};
+
+export const actions = {
+};
+
+
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state,
+  mutations,
+  actions,
 });
---
+++ tests/unit/store.spec.js
@@ -0,0 +1,23 @@
+import { state, BLACK } from '@/store';
+
+describe('store', () => {
+  describe('state', () => {
+    it('should contain board with initial data', () => {
+      const expected = [
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 2, 0, 0, 0,
+        0, 0, 0, 2, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
+      ];
+      expect(state.board).toEqual(expected);
+    });
+
+    it('should contain current player with black as first player', () => {
+      expect(state.currentPlayer).toEqual(BLACK);
+    });
+  });
+});
