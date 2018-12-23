e672a75695ac6a69ebb44309159b189f2193971a
Alexis Couronne <alexis@skitoo.net>
2018-12-23 11:43:53 +0100

    feat: add Piece component

---
+++ src/components/Piece.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="piece"
+    :class="{ white, black }">
+  </div>
+</template>
+
+<script>
+import { WHITE, BLACK } from '@/store';
+
+export default {
+  props: {
+    color: {
+      required: true,
+      type: Number,
+    },
+  },
+  computed: {
+    white() {
+      return this.color === WHITE;
+    },
+    black() {
+      return this.color === BLACK;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.piece {
+  width: 80%;
+  height: 80%;
+  border-radius: 50%;
+  box-shadow: 3px 3px 0 #555;
+  margin: auto;
+}
+.white {
+  background: #fff;
+}
+.black {
+  background: #000;
+}
+</style>
---
+++ tests/unit/piece.spec.js
@@ -0,0 +1,22 @@
+import { shallowMount } from '@vue/test-utils';
+import Piece from '@/components/Piece.vue';
+import { WHITE, BLACK } from '@/store';
+
+describe('Piece.vue', () => {
+  it('renders piece with white color', () => {
+    const wrapper = shallowMount(Piece, {
+      propsData: {
+        color: WHITE,
+      },
+    });
+    expect(wrapper.contains('.white')).toBeTruthy();
+  });
+  it('renders piece with black color', () => {
+    const wrapper = shallowMount(Piece, {
+      propsData: {
+        color: BLACK,
+      },
+    });
+    expect(wrapper.contains('.black')).toBeTruthy();
+  });
+});
